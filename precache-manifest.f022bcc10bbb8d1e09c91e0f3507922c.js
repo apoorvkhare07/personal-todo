313613a3d9b5de255c2b5a842f60c8965af5613f
apoorv <apoorvkhare007@gmail.com>
2020-05-18 20:43:46 +0530

    Updates

---
+++ precache-manifest.f022bcc10bbb8d1e09c91e0f3507922c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f1c8cf71cfc4d693e681580c270dd",
+    "url": "/personal-todo/index.html"
+  },
+  {
+    "revision": "5a54b81095338c5afdd8",
+    "url": "/personal-todo/static/css/main.513d72d5.chunk.css"
+  },
+  {
+    "revision": "eca5c032fc3f910be6c3",
+    "url": "/personal-todo/static/js/2.8e4bc234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-todo/static/js/2.8e4bc234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a54b81095338c5afdd8",
+    "url": "/personal-todo/static/js/main.ee39ba8f.chunk.js"
+  },
+  {
+    "revision": "16298f1eab03147626bb",
+    "url": "/personal-todo/static/js/runtime-main.8c7145d5.js"
+  }
+]);
